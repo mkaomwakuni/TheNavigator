fca7dc58108cf57af7baa7326c41aec18ca5ea65
mkao <mkaoibrahim@gmail.com>
2022-10-10 22:23:25 +0300

    Request Permissions

--- app/src/main/java/com/mkao/thenavigator/MapsActivity.kt
+++ app/src/main/java/com/mkao/thenavigator/MapsActivity.kt
@@ -1,8 +1,14 @@
 package com.mkao.thenavigator
 
+import android.Manifest
+import android.app.Activity
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -47,5 +53,31 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+    object LocationHelperPermissions {
+        private const val  BACKGROUND_LOCATION_PERMISSION = Manifest.permission.ACCESS_BACKGROUND_LOCATION
+        private const val  COARSE_LOCATION_PERMISSION = Manifest.permission.ACCESS_COARSE_LOCATION
+        private const val FINE_LOCATION_PERMISSION = Manifest.permission.ACCESS_FINE_LOCATION
+        fun hasPermissionManager (activity: Activity): Boolean {
+            return ContextCompat.checkSelfPermission(activity, FINE_LOCATION_PERMISSION) ==PackageManager.PERMISSION_GRANTED
+                    && ContextCompat.checkSelfPermission(activity, BACKGROUND_LOCATION_PERMISSION)==PackageManager.PERMISSION_GRANTED
+        }
+        fun requestPermissions(activity: Activity){
+            if (ActivityCompat.shouldShowRequestPermissionRationale(activity,
+                    FINE_LOCATION_PERMISSION)){
+                AlertDialog.Builder(activity).apply {
+                    setMessage(activity.getString(R.string.permission_required))
+                    setPositiveButton(activity.getString(R.string.ok)){
+                        _,_ ->
+                        ActivityCompat.requestPermissions(activity, arrayOf(FINE_LOCATION_PERMISSION,
+                            COARSE_LOCATION_PERMISSION, BACKGROUND_LOCATION_PERMISSION),0)
+                    }
+                    show()
+                }
+            }else{
+                ActivityCompat.requestPermissions(activity, arrayOf(FINE_LOCATION_PERMISSION,
+                    COARSE_LOCATION_PERMISSION, BACKGROUND_LOCATION_PERMISSION),0)
+            }
+        }
     }
 }
