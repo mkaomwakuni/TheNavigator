bf3c77832ccbba3e47ed8a508d664f2c11b28333
mkao <mkaoibrahim@gmail.com>
2022-10-12 22:14:28 +0300

    Access points Recently

--- app/src/main/java/com/mkao/thenavigator/MapsActivity.kt
+++ app/src/main/java/com/mkao/thenavigator/MapsActivity.kt
@@ -1,14 +1,18 @@
 package com.mkao.thenavigator
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.pm.PackageManager
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -21,6 +25,9 @@ import com.mkao.thenavigator.databinding.ActivityMapsBinding
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
+    //finds the user locality and prepares the map view
+    private lateinit var recentLastLocation :Location
+    private lateinit var fusedLocation: FusedLocationProviderClient
     private lateinit var binding: ActivityMapsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,9 +42,11 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+        fusedLocation = LocationServices.getFusedLocationProviderClient(this)
         // check whether the location permissions have been granted each time  app launches
         if (!LocationHelperPermissions.hasPermissionManager(this))
             LocationHelperPermissions.requestPermissions(this)
+
     }
         //user’s response to the permissions request
     override fun onRequestPermissionsResult(
@@ -52,8 +61,20 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             prepareMap()
     }
 
+    @SuppressLint("MissingPermission")
     private fun prepareMap() {
-        TODO("Not yet implemented")
+      if (LocationHelperPermissions.hasPermissionManager(this)){
+          mMap.isMyLocationEnabled = true
+          //Locate last known locality
+          fusedLocation.lastLocation.addOnSuccessListener { location:Location?
+          ->
+              location?.apply {
+                  recentLastLocation = location
+                  val currentLatitude = LatLng(location.latitude,location.longitude)
+                  mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatitude, 20f))
+              }
+          }
+      }
     }
 
     /**
